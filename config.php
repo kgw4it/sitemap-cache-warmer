ce7f8c7a2d2b80774366d2e8f5d817d8bfe94e80
kgw4it <30734393+kgw4it@users.noreply.github.com>
2019-08-06 15:30:22 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
 return array(
     'key' => $_ENV['SECRET_KEY'], // Secret key to allow traversing sitemaps
+    'reportProblematicUrls' => strlen($_ENV['REPORT_PROPLEMATIC_URLS_TOMAIL']) > 0 ? true : false,
+    'reportProblematicUrlsTo' => $_ENV['REPORT_PROPLEMATIC_URLS_TOMAIL'],
 );
