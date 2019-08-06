6e87439725fc2c90daa9ce7e7ea771a8d30f7607
kgw4it <30734393+kgw4it@users.noreply.github.com>
2019-08-06 16:05:38 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-    'key' => $_ENV['SECRET_KEY'], // Secret key to allow traversing sitemaps
-    'reportProblematicUrls' => strlen($_ENV['REPORT_PROPLEMATIC_URLS_TOMAIL']) > 0 ? true : false,
-    'reportProblematicUrlsTo' => $_ENV['REPORT_PROPLEMATIC_URLS_TOMAIL'],
-    'SMTP_HOST' => $_ENV['SMTP_HOST'],
-    'SMTP_PORT' => $_ENV['SMTP_PORT'],
-    'SMTP_MAIL_FROM' => $_ENV['SMTP_MAIL_FROM'],
+    'key' => getenv('SECRET_KEY'), // Secret key to allow traversing sitemaps
+    'reportProblematicUrls' => strlen(getenv('REPORT_PROPLEMATIC_URLS_TOMAIL')) > 0 ? true : false,
+    'reportProblematicUrlsTo' => getenv('REPORT_PROPLEMATIC_URLS_TOMAIL'),
+    'SMTP_HOST' => getenv('SMTP_HOST'),
+    'SMTP_PORT' => getenv('SMTP_PORT'),
+    'SMTP_MAIL_FROM' => getenv('SMTP_MAIL_FROM'),
 );
